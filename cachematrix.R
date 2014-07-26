897d1b25afd2f0400d7a171763f2597fe35e464f
Michael Breecher <michaelbreecher@gmail.com>
2014-07-26 10:43:12 -0700

    first pass at assignment 2 solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+# Create a matrix wrapper that allows the inverse of the 
+# matrix to be stored in the global environment
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  # verify input is correct type
+  if(!is.matrix(x)){
+    stop("Argument needs to be a matrix")
+  }
+  
+  inverse <- NULL # set empty spot for inverse
+  
+  # function to set matrix and inverse in global environment
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  #function to get matrix and inverse
+  get <- function() x
+  #solve matrix and store solution in global environment
+  setinverse <- function(solve) inverse <<- soln
+  getinverse <- function() inverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+# calculates the inverse of the matrix object
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # grab getinverse attribute from matrix object
+  result <- x$getinverse()
+  
+  # if the inverse has been calculated, get it
+  if(!is.null(result)) {
+    message("getting cached data")
+    return(result)
+  }else{ #otherwise, set the attributes of the matrix object (including the inverse)
+    input <- x$get()
+    result <- solve(input, ...)
+    x$setinverse(result)
+  }
+  
+  #return matrix inverse 
+  result
 }
